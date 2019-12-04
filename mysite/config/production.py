f279a819748bc6907a9bbef49b8fc6b20e1a3eab
Sure <sure.z@outlook.com>
2019-12-04 23:03:38 +0800

    部署2

--- mysite/config/production.py
+++ mysite/config/production.py
@@ -3,4 +3,6 @@ from .base import *
 SECRET_KEY = 'd+h0io02_a8=g#v9&4adwkbbtkn!usqd3fhn9xf(g7a-ln32al'
 
 DEBUG = False
-SITE_HOST = ['shoor.xyz', ]
+SITE_HOST = 'shoor.xyz'
+
+ALLOWED_HOSTS = ['shoor.xyz']
